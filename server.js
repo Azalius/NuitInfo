4201f991d19db42e99b9bdace75ef46e98bd34e0
Flchs <francois@laptop-francois.localdomain>
2018-12-07 00:16:48 +0100

    API routers

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const routes = require('./routes/routes.js');
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/src/index.html'));
 });
+
+app.use('/api', routes);
 
 app.use('/ressources', express.static('ressources'));
 

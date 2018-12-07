97f503e203a6eac5af12f1cbe9be81eabde84716
Flchs <francois@laptop-francois.localdomain>
2018-12-07 01:56:56 +0100

    Db access

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+//const connection = require('../db/db')
 
-// define the about route
-router.get('/test', function(req, res) {
-  res.send('salut');
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: '51.75.90.197',
+  user: '',
+  password: '',
+  database: 'namib'
 });
+
+connection.connect();
+
+data = {};
+
+
+
+
+router.get('/personne/:id', function(req, res) {
+  let id = req.params.id;
+  connection.query('SELECT * FROM `personne` WHERE id = ' + id, function(error, results, fields) {
+    if (error) throw error;
+    data.personne = results[0];
+  });
+  res.send(data);
+});
+
+
 
 module.exports = router;

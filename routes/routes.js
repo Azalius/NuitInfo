df3be24737b2f2e071e99f28670fdccac23f446a
Thygams <Thygams@users.noreply.github.com>
2018-12-07 02:00:08 +0100

    correction conflits

--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,8 @@
-<<<<<<< HEAD
-var express = require('express');
-var router = express.Router();
-//const connection = require('../db/db')
-=======
 const express = require('express');
 const router = express.Router();
->>>>>>> fee0801e3b6ead0abc1d1b5ed969e74e2e8797e5
 
-var mysql = require('mysql');
-var connection = mysql.createConnection({
+const mysql = require('mysql');
+const connection = mysql.createConnection({
   host: '51.75.90.197',
   user: '',
   password: '',
@@ -18,9 +12,6 @@ var connection = mysql.createConnection({
 connection.connect();
 
 data = {};
-
-
-
 
 router.get('/personne/:id', function(req, res) {
   let id = req.params.id;
